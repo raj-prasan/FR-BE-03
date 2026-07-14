dc4d704645d5382ea6149d6bef2fb04192f259ce
raj-prasan <rajprasanj@gmail.com>
2026-07-14 14:13:54 +0530

    feat: add Swagger UI documentation and update API structure

--- app.ts
+++ app.ts
@@ -1,16 +1,32 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from 'cookie-parser'
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "./openapi.json" with { type: "json" };
 
-import { taskRouter } from "./routes/task.route.js"
+import { taskRouter } from "./routes/task.route.js";
 
+const app = express();
+app.use(cors());
 
-const app = express()
-app.use(cors())
-
-app.use(express.json({limit: "16Kb"}))
+app.use(express.json({ limit: "16Kb" }));
 app.use(cookieParser());
 
-app.use("/tasks", taskRouter)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-export {app};
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"],
+  });
+});
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "ok"
+  });
+});
+
+app.use("/tasks", taskRouter);
+
+export { app };
