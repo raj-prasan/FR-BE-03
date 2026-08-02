e3d91c2e5f8bb2b834d1e68da24423bd733500da
raj-prasan <rajprasanj@gmail.com>
2026-08-03 00:37:50 +0530

    Stage 4: docker-compose the whole stack

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-import { app } from "./app.js";
 import dotenv from "dotenv";
+dotenv.config();
+import { app } from "./app.js";
+
 import { seed } from "./seed.js";
 
-dotenv.config();
+
 
 const PORT = process.env.PORT;
 
@@ -12,6 +14,6 @@ try {
     console.error(error)
 } finally {
   app.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT}`);
+    console.log(`Server is running on port : ${PORT || 3000}`);
   });
 }
--- seed.ts
+++ seed.ts
@@ -1,4 +1,6 @@
 import {Client, Pool} from "pg"
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL
